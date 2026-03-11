648964d7af36e47fe2619972b93e27cb0c689297
Samira Salimiyan <szs0394@auburn.edu>
2026-03-10 21:58:27 -0500

    Applied Python script best practices

--- smallest_factor.py
+++ smallest_factor.py
@@ -1,22 +1,44 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
-# A script for getting the smallest prime factor of an integer.
+"""
+Script to compute the smallest prime factor of a given positive integer.
+
+Usage:
+    python script.py <integer>
+
+Example:
+    python script.py 15
+    Output: 3
+"""
 
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
-n = int(sys.argv[1])
-if n < 1:
-    sys.exit(sys.argv[0] + ": Expecting a positive integer")
 
-x = None
-for i in range(2, n):
-    if (n % i) == 0:
-        x = i
-        break
+def smallest_prime_factor(n):
+    """Return the smallest prime factor of n."""
+    for i in range(2, n):
+        if n % i == 0:
+            return i
+    return n
 
-if x is None:
-    print(n)
-else:
-    print(x)
+
+def main():
+    """Parse arguments and run the program."""
+    
+    if len(sys.argv) != 2:
+        sys.exit(f"{sys.argv[0]}: Expecting one command line argument -- the integer to factor")
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        sys.exit("Error: Argument must be an integer")
+
+    if n < 1:
+        sys.exit(f"{sys.argv[0]}: Expecting a positive integer")
+
+    result = smallest_prime_factor(n)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
